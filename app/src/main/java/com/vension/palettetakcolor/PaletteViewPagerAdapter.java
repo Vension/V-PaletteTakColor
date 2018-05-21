361e1a5e02e971fe988308f79f44fdeb9cc6a871
vension <hqw@kewaimiao.com>
2018-05-21 16:32:09 +0800

    palette take color Initial commit

---
+++ app/src/main/java/com/vension/palettetakcolor/PaletteViewPagerAdapter.java
@@ -0,0 +1,33 @@
+package com.vension.palettetakcolor;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class PaletteViewPagerAdapter extends FragmentPagerAdapter {
+
+    final int PAGE_COUNT = 5;
+    private String tabTitles[] = new String[]{"主页", "分享", "收藏", "关注", "微博"};
+    private Context context;
+
+    public PaletteViewPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return PaletteFragment.newInstance(position);
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabTitles[position];
+    }
+}
